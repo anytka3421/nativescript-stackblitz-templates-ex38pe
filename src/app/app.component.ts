c3686842cf7e80dd1d64bea3c5c49e401911bec5
anytka3421 <119698653+anytka3421@users.noreply.github.com>
2023-06-29 16:04:48 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  items = [
+    { name: 'Item 1', description: 'Description 1' },
+    { name: 'Item 2', description: 'Description 2' },
+    { name: 'Item 3', description: 'Description 3' },
+  ];
+}
